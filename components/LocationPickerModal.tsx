27588eebf6caba1d13eb7fb4f7980c82a9f9799a
Commitplusx <calebvazquez2412@gmail.com>
2025-11-13 19:25:14 -0700

    cambios al modal

--- components/LocationPickerModal.tsx
+++ components/LocationPickerModal.tsx
@@ -115,6 +115,131 @@ export const LocationPickerModal: React.FC<LocationPickerModalProps> = ({
               options={{
                 disableDefaultUI: true,
                 zoomControl: true,
+                gestureHandling: 'greedy', // Enable single-finger panning on mobile
+                styles: [
+                  {
+                    "elementType": "geometry",
+                    "stylers": [
+                      { "color": "#f5f5f5" }
+                    ]
+                  },
+                  {
+                    "elementType": "labels.icon",
+                    "stylers": [
+                      { "visibility": "off" }
+                    ]
+                  },
+                  {
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                      { "color": "#616161" }
+                    ]
+                  },
+                  {
+                    "elementType": "labels.text.stroke",
+                    "stylers": [
+                      { "color": "#f5f5f5" }
+                    ]
+                  },
+                  {
+                    "featureType": "administrative.land_parcel",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                      { "color": "#bdbdbd" }
+                    ]
+                  },
+                  {
+                    "featureType": "poi",
+                    "elementType": "geometry",
+                    "stylers": [
+                      { "color": "#eeeeee" }
+                    ]
+                  },
+                  {
+                    "featureType": "poi",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                      { "color": "#757575" }
+                    ]
+                  },
+                  {
+                    "featureType": "poi.park",
+                    "elementType": "geometry",
+                    "stylers": [
+                      { "color": "#e5e5e5" }
+                    ]
+                  },
+                  {
+                    "featureType": "poi.park",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                      { "color": "#9e9e9e" }
+                    ]
+                  },
+                  {
+                    "featureType": "road",
+                    "elementType": "geometry",
+                    "stylers": [
+                      { "color": "#ffffff" }
+                    ]
+                  },
+                  {
+                    "featureType": "road.arterial",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                      { "color": "#757575" }
+                    ]
+                  },
+                  {
+                    "featureType": "road.highway",
+                    "elementType": "geometry",
+                    "stylers": [
+                      { "color": "#dadada" }
+                    ]
+                  },
+                  {
+                    "featureType": "road.highway",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                      { "color": "#616161" }
+                    ]
+                  },
+                  {
+                    "featureType": "road.local",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                      { "color": "#9e9e9e" }
+                    ]
+                  },
+                  {
+                    "featureType": "transit.line",
+                    "elementType": "geometry",
+                    "stylers": [
+                      { "color": "#e5e5e5" }
+                    ]
+                  },
+                  {
+                    "featureType": "transit.station",
+                    "elementType": "geometry",
+                    "stylers": [
+                      { "color": "#eeeeee" }
+                    ]
+                  },
+                  {
+                    "featureType": "water",
+                    "elementType": "geometry",
+                    "stylers": [
+                      { "color": "#c9c9c9" }
+                    ]
+                  },
+                  {
+                    "featureType": "water",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                      { "color": "#9e9e9e" }
+                    ]
+                  }
+                ]
               }}
             >
               {/* Marcador fijo en el centro */}
@@ -126,7 +251,7 @@ export const LocationPickerModal: React.FC<LocationPickerModalProps> = ({
               transform: 'translate(-50%, -100%)', // Ajusta para que la punta del marcador esté en el centro
               zIndex: 1
             }}>
-              <FaMapMarkerAlt color="red" size={40} />
+              <FaMapMarkerAlt color="#f97316" size={50} />
             </div>
           </>
         )}
