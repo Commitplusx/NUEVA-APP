e27989d0e6a85d166efea691f71b9f8442ff90b5
Commitplusx <calebvazquez2412@gmail.com>
2025-11-14 19:21:02 -0700

    generando apk

---
+++ android/app/src/main/java/estrella/app/shop/MainActivity.java
@@ -0,0 +1,5 @@
+package estrella.app.shop;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'estrella.app.shop',
+  appName: 'App Estrella',
+  webDir: 'dist'
+};
+
+export default config;
